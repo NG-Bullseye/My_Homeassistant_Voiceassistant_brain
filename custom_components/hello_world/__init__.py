2e4c2ac5fac8b0a1870e071c7d6b0c421b98fd0b
NG-Bullseye <leonard.r.wecke@gmail.com>
2024-07-20 18:34:40 +0200

    Fixing

--- custom_components/hello_world/__init__.py
+++ custom_components/hello_world/__init__.py
@@ -1,7 +1,4 @@
-import logging
 from homeassistant.helpers import intent
-
-_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
     hass.helpers.intent.async_register(MyIntentHandler())
@@ -11,8 +8,4 @@ class MyIntentHandler(intent.IntentHandler):
     intent_type = 'HelloWorldIntent'
 
     async def handle(self, intent_obj):
-        _LOGGER.info("Handled intent: %s", intent_obj.intent_type)
-        response = intent_obj.create_response()
-        response.async_set_speech("Hello World")
-        response.async_set_card("Intent handled successfully")
-        return response
+        # Your intent handling logic here
